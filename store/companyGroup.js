4a081408949098e5aea681ed695987b7d231f012
vladislavbalovnev <70537419+vladislavbalovnev@users.noreply.github.com>
2021-02-12 10:42:33 +0300

    all done

--- store/companyGroup.js
+++ store/companyGroup.js
@@ -1,28 +1,28 @@
 
 export const state = () => ({
-    companyGroup: []
+  companyGroup: []
 })
 
 export const getters = {
-    getCompanyGroup: state => state.companyGroup
+  getCompanyGroup: state => state.companyGroup
 }
 
 export const mutations = {
-    setCompanyGroup(state, group) {
-      state.companyGroup.push(group.details);
-    }
+  setCompanyGroup(state, group) {
+    state.companyGroup.push(group.details);
+  }
 }
 
 export const actions = {
-    async setGroup({commit}) {
-      try {
-        const accessToken = "access_token=r27iavtafltg4e18dc265dd6a6452d21d89771b7deb6435";
-        const json = "json=true";
-        const connectionString = 'https://vsem-edu-oblako.ru/adminapi/api/getListCampaign' + `?${accessToken}` + `&${json}`;
-        const group = await this.$axios.$get(connectionString);
-        commit('setCompanyGroup', group);
-      } catch(e) {
-        console.error(e.message);
-      }
+  async setGroup({commit}) {
+    try {
+      const accessToken = "access_token=r27iavtafltg4e18dc265dd6a6452d21d89771b7deb6435";
+      const json = "json=true";
+      const connectionString = 'https://vsem-edu-oblako.ru/adminapi/api/getListCampaign' + `?${accessToken}` + `&${json}`;
+      const group = await this.$axios.$get(connectionString);
+      commit('setCompanyGroup', group);
+    } catch(e) {
+      console.error(e.message);
     }
+  }
 }
